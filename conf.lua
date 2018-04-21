3773a3a5054caf77a4edbe1dbb1200c4bd9f5812
Mike Minassian <minassbond@gmail.com>
2018-04-21 12:13:09 -0400

    Console added to conf

--- conf.lua
+++ conf.lua
@@ -1,9 +1,5 @@
 function love.conf(t)
     t.window.width = 480
     t.window.height = 720
-<<<<<<< HEAD
     t.console=true
-=======
-    t.console = true
->>>>>>> 193bd3ac8ff6949c5361a29222271b6a5ccebe49
 end
